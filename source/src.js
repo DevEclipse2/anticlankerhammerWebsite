d65c18646f192e9fe2da4347b2d749dfadacb14d
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-05-15 18:38:11 +0800

    NO ONE LIKES JAVA SCRIPT

---
+++ source/src.js
@@ -0,0 +1,9 @@
+
+//java is bad enough. now you're saying theres ANOTHER ONE????????????
+//goys use javascript
+    const heading = document.getElementById('message');
+      
+    // Changing the text content via JS
+    heading.textContent = 'Hello World!';        
+    // Logging it to the browser console as well
+   console.log('JavaScript says: Hello World!');
