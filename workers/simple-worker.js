cb5ae1cb1365af1155594853458ed97c639bf722
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-05-22 17:10:03 +0800

    new official webpage

--- workers/simple-worker.js
+++ workers/simple-worker.js
@@ -1,14 +1,43 @@
-const PUSHER_CLUSTER = env.PUSHER_CLUSTER;
+//copied from cloudflare worker editor
 
-export default{
-    async fetch (request){
-        //takes in a request
-        if(request.method !== "POST"){ return new Response("Method is invalid",{status : 405});}
-        else
+import Pusher from "pusher";
+
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    const allowedPath = ".anticlankerhammer.org";
+    const allowedOrigin = "*"; //everything allowed in, except we don't
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        headers: {
+          "Access-Control-Allow-Origin": allowedOrigin,
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+      });
+    }
+
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": allowedOrigin,
+      "Content-Type": "application/json",
+    };
+
+    //this is part of the allowed path
+    if (url.href.indexOf(allowedPath)!= -1) {
+        
+        if(request.message == "new-room")
         {
-            return new Response("hello again dumbbell!");
-
+            
         }
         
+      return new Response(JSON.stringify({ message: "Access granted" }), {
+        status: 200,
+        headers: corsHeaders,
+      });
     }
-}
+    return new Response(JSON.stringify({ error: "Access Denied" }), {
+      status: 403,
+      headers: corsHeaders,
+    });
+  },
+};
