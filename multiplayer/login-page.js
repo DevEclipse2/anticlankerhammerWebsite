9c49cc3057a98a2715c0e24c559b1ce6cbb113a3
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-05-25 23:41:11 +0800

    login frontend page

---
+++ multiplayer/login-page.js
@@ -0,0 +1,47 @@
+const loginWorker = "";
+const btn = document.getElementById("login");
+btn.addEventListener("click", LoginToWebsite);
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const text = document.getElementById("login-header");
+const original = text.textContent;
+var canPress = true;
+
+async function LoginToWebsite()
+{
+    if(canPress)
+    {
+        canPress = false;
+        text.textContent = "attempting to contact palantir and related 3-letter organizations, please wait...";
+        //check login
+
+
+        setTimeout(() => {
+            text.textContent = original;
+            alert("operation timed out after 30 seconds. Did i forget to pay the bills again?");
+        }, 30000);
+        setTimeout(() => {
+            canPress = true;
+        }, 12000);
+
+
+        var data = {o: "join" ,n : dispname.value,c : roomcode.value.toUpperCase()};
+        try {
+            const response = await fetch(WORKER_URL, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            });
+            const data = await response.json();
+            readReturnData(data);
+            console.log('Worker replied:', data);
+        } catch (error) {
+            alert('Error:', error);
+            canPress = true;
+        }
+    }
+    else
+    {
+        alert("Everytime you spam a request, i send 500k to israel");
+    }
+}
