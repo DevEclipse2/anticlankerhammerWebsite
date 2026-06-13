7ad5af379c59d60cf1e1680c22b46d18896c03b0
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-06-13 09:26:20 +0800

    i forogto to hash the passwords
    sone imcrine

--- multiplayer/register-worker.js
+++ multiplayer/register-worker.js
@@ -33,19 +33,20 @@ export default {
                     });
                 }
                 //run email format checks
-                //hash the email
+                //hash the password
                 const saltArray = crypto.getRandomValues(new Uint8Array(16));
                 const saltHex = Array.from(saltArray).map(b => b.toString(16).padStart(2, '0')).join('');
+                const secureHash = hashPassword(password,saltArray);
                 if(reg_email)
                 {
                     await env.DB.prepare('INSERT INTO users (username,email, hash , salt) VALUES (?1, ?2, ?3, ?4)')
-                        .bind(username, email,password,saltHex)
+                        .bind(username, email,secureHash,saltHex)
                         .run();
                 }
                 else
                 {
                     await env.DB.prepare('INSERT INTO users (username, hash , salt) VALUES (?1, ?2, ?3)')
-                        .bind(username, password, saltHex)
+                        .bind(username, secureHash, saltHex)
                         .run();
                 }
                 return new Response(JSON.stringify({ message: "user added successfully!" }), { 
@@ -83,4 +84,33 @@ export default {
         
         
     },
+
+ 
+}
+ async function hashPassword(password, saltArray)
+{
+    const encoder = new TextEncoder();
+    const passwordBuffer = encoder.encode(password);
+    
+
+    // Import the password as a base key
+    const importedKey = await crypto.subtle.importKey(
+        "raw", 
+        passwordBuffer, 
+        { name: "PBKDF2" }, 
+        false, 
+        ["deriveBits"]
+    );
+
+    // Run the PBKDF2 algorithm 
+    const hashBuffer = await crypto.subtle.deriveBits(
+        { name: "PBKDF2", salt: saltArray, iterations: 121000, hash: "SHA-256" },
+        importedKey, 
+        256
+    );
+
+    // Convert the final secure hash into a Hex string for database storage
+    return Array.from(new Uint8Array(hashBuffer))
+        .map(b => b.toString(16).padStart(2, '0'))
+        .join('');
 }
